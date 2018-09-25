992a05f8cd8ae10571683a1e552c293d9c90f27c
Orlando Diziro Neto <zxorlando@gmail.com>
2018-09-25 13:14:31 -0300

    Exclusao

--- DAO/class/Usuario.php
+++ DAO/class/Usuario.php
@@ -115,6 +115,20 @@ class Usuario {
 
 	}
 
+	public function delete(){
+
+		$sql = new Sql();
+
+		$sql->query("DELETE FROM tb_usuarios WHERE idusuario = :ID", array(':ID'=>$this->getIdusuario()));
+
+		$this->setIdusuario(NULL);
+		$this->setDeslogin("");
+		$this->setDessenha("");
+		$this->setDtcadastro(new DateTime());
+
+
+	}
+
 	public function __construct($login="", $password=""){
 
 		$this->setDeslogin($login);
--- DAO/index.php
+++ DAO/index.php
@@ -38,12 +38,17 @@ $aluno->insert();
 echo $aluno;
 */
 
+/*
 $usuario = new Usuario();
 $usuario->loadById(6);
 $usuario->update("professor", "!@#$%");
-
 echo $usuario;
+*/
 
+$usuario = new Usuario();
+$usuario->loadById(5);
+$usuario->delete();
+echo $usuario;
 
 
  ?>
