a96c8900273db0f5f4ee56786860cbf098960be1
Orlando Diziro Neto <zxorlando@gmail.com>
2018-09-25 13:08:49 -0300

    inserir e alterar

--- DAO/index.php
+++ DAO/index.php
@@ -18,8 +18,32 @@ $search = Usuario::search("jo");
 echo json_encode($search);
 */
 
+/*
 $usuario = new Usuario();
 $usuario->login("jose","1234567890");
 echo $usuario;
+*/
+
+/*
+$aluno = new Usuario();
+$aluno->setDeslogin("aluno");
+$aluno->setDessenha("@lun0");
+$aluno->insert();
+echo $aluno;
+*/
+
+/*
+$aluno = new Usuario("aluno", "@lun0");
+$aluno->insert();
+echo $aluno;
+*/
+
+$usuario = new Usuario();
+$usuario->loadById(6);
+$usuario->update("professor", "!@#$%");
+
+echo $usuario;
+
+
 
  ?>
